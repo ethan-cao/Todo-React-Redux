7ba4a521f86f002af750abb21af080c1524f9d2a
guane <Ethan.GuangxueCao@pega.com>
2019-01-22 21:36:48 +0000

    sync

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 // GET home page
-router.get('/', function(req, res, next) {
+router.get('/api/index', function(req, res, next) {
   console.log("@@@@ server index router");
   res.render('index', { title: 'Ethan' });
 });
