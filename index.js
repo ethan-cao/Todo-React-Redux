7b79327883833a2ef1d0c6f7e1a25652b7c0b1d3
guane <Ethan.GuangxueCao@pega.com>
2019-06-28 18:58:36 +0000

    start point

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const path = require('path');
 const express = require("express"); 
 
-// start app
 var app = express();
-
-console.log("@@@ path: " + path.join(__dirname, 'client', 'build'));
-
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-app.get('/', (req, res) => {
-  res.send("deployed");
-});
 
 // EB runs NodeJS application at port 8081 by default 
 const port = process.env.PORT || 8081;
 app.listen(port, () => {
   console.log("Server listening at port " + port);
+});
+
+const staticPath = path.join(__dirname, 'client', 'build');
+app.use(express.static(staticPath));
+// console.log("@@@ path: " + staticPath );
+
+app.get("*", (req, res) => {
+  res.send("deployed");
+  // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
