e925068acb37dff977a75b95f7e199b03ae32d8d
guane <Ethan.GuangxueCao@pega.com>
2019-06-28 19:31:18 +0000

    udpate

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,6 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";  
 
-// const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(reduxThunk)));
 
